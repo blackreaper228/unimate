f78942107f73de40cb70368cc7936b3cb256f8e9
blackreaper228 <ntsvetnikov@gmail.com>
2025-05-14 06:26:48 +0300

    start

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-import './style.css';
+import "./stylesheets/reset.css";
+import "./stylesheets/fonts.css";
+import "./stylesheets/basics.css";
+import "./stylesheets/style.css";
+import "./stylesheets/typekit.css";
